bb6a5ac4b8a3c59f51ea3122278a6040a735fbf3
tina <tinapayno2@gmail.com>
2025-01-23 06:02:59 +0330

    added plant class

---
+++ app/src/main/java/com/first/unjumbleit/Planet.kt
@@ -0,0 +1,7 @@
+package com.first.unjumbleit
+
+data class Planet(
+    val name: String, // Planet name
+    val imageResId: Int, // Image resource ID for the planet
+    val planetName: String // Planet name (matches database)
+)
