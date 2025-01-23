82d66245953a26ece74a442923ff6246e5ffeeb6
tina <tinapayno2@gmail.com>
2025-01-24 02:11:35 +0330

    updated activity for gif

--- app/src/main/java/com/first/unjumbleit/SplashScreenActivity.kt
+++ app/src/main/java/com/first/unjumbleit/SplashScreenActivity.kt
@@ -1,23 +1,32 @@
 package com.first.unjumbleit
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import android.os.Handler
+import android.os.Looper
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 
-class SplashScreenActivity : ComponentActivity() {
+class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        // Navigate to StartPageActivity after 3 seconds using coroutines
-        lifecycleScope.launch {
-            delay(3000)  // 3-second delay
-            val intent = Intent(this@SplashScreenActivity, StartPageActivity::class.java)
-            startActivity(intent)
-            finish()  // End the SplashScreenActivity
+        val splashVideoView = findViewById<VideoView>(R.id.splashVideoView)
+        val videoUri = Uri.parse("android.resource://${packageName}/${R.raw.portal}")
+        splashVideoView.setVideoURI(videoUri)
+        splashVideoView.start()
+
+        splashVideoView.setOnCompletionListener {
+            startActivity(Intent(this, StartPageActivity::class.java))
+            finish()
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, StartPageActivity::class.java))
+            finish()
+        }, 3000) // 5 seconds fallback timeout
     }
 }
